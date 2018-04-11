958ee19e2e5a184e1a1b32031206b759d3bc14d8
Zsolt Meszaros <zsolt@sowad.com>
2018-04-11 11:06:46 +0100

    Add validation to author routes

--- server/routes/authors.js
+++ server/routes/authors.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const validate = require('express-validation');
+
 const authorController = require('../controllers/authors');
+const validations = require('./validation/authors');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(authorController.list)
-  .post(authorController.create);
+  .post(validate(validations.createAuthor), authorController.create);
 
 router
   .route('/:authorId')
   .get(authorController.get)
-  .put(authorController.update)
+  .put(validate(validations.updateAuthor), authorController.update)
   .delete(authorController.remove);
 
-router.route('/search/:query/:num?').get(authorController.search);
+router
+  .route('/search/:query/:num?')
+  .get(validate(validations.search), authorController.search);
 
+router.param('authorId', validate(validations.getAuthor));
 router.param('authorId', authorController.load);
 
 module.exports = router;
