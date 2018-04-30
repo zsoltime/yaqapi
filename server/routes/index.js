48fba7277c1f3bf91119a2e7d5114b778413526f
Zsolt Meszaros <zsolt@sowad.com>
2018-04-30 21:41:10 +0100

    Add quote routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 
 const authorRoutes = require('./authors');
 const categoryRoutes = require('./categories');
+const quoteRoutes = require('./quotes');
 const notFound = require('./notFound');
 
 const router = express.Router();
 
 router.use('/authors', authorRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/quotes', quoteRoutes);
 router.use(notFound);
 
 module.exports = router;
